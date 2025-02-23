a425a019b5f57da190290fe8900255d4848cc63f
Blair Johnson <blairjohnson@gatech.edu>
2025-02-23 16:21:20 -0500

    ugly, but we're ranging baby

--- main/footpad.cpp
+++ main/footpad.cpp
@@ -33,29 +33,35 @@ extern "C" void app_main()
   Wire.setPins(SDA_PIN, SCL_PIN);
   Wire.begin();
   if (! vl53.begin(SENSOR_ADDRESS_0, &Wire)) {
-    ESP_LOGI("WIRE", "Error on init of VL sensor: %d\n", vl53.vl_status);
+    ESP_LOGI("APP_MAIN", "Error on init of VL sensor: %d\n", vl53.vl_status);
     while (1)       delay(10);
   }
-  ESP_LOGI("WIRE", "vl53 begun.\n");
+  ESP_LOGI("APP_MAIN", "vl53 begun. Sensor id: %#x\n", vl53.sensorID());
+
+  if (!vl53.startRanging()) {
+      ESP_LOGI("APP_MAIN", "Couldn't begin ranging, error code: %d\n", vl53.vl_status);
+      while (1)     delay(10);
+  }
+  ESP_LOGI("APP_MAIN", "Ranging started.\n");
 
   vl53.setTimingBudget(50);
   ESP_LOGI("APP_MAIN", "vl53 timing budget set: %d\n", vl53.getTimingBudget());
 
 
   // Arduino-like loop()
+  int16_t distance;
   while(true){
-      int16_t distance;
-
       if (vl53.dataReady()){
           distance = vl53.distance();
-          if (distance != -1){
+          if (distance == -1){
               ESP_LOGI("LOOP", "Couldn't get distance.\n");
               ESP_LOGI("LOOP", "Error code: %d\n", vl53.vl_status);
           }
           ESP_LOGI("LOOP", "Distance: %dmm\n", distance);
           vl53.clearInterrupt();
       }
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      distance = -1;
+      vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 
   // WARNING: if program reaches end of function app_main() the MCU will restart.
